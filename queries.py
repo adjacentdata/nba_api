195e44a58216ae3ea72261bc02f243c2f190e67b
AJ Perez <atpcodeio@gmail.com>
2022-11-19 00:44:02 -0800

    Created players class

--- queries.py
+++ queries.py
@@ -1,14 +1,14 @@
-CREATE_NBA_PLAYERS_TABLE = ("""
-CREATE TABLE IF NOT EXISTS players 
-(PLAYER_NAME varchar(255),
-TEAM_ID int,
-PLAYER_ID int,
-SEASON int)
-""")
+class Players:
+    CREATE_NBA_PLAYERS_TABLE = ("""
+        CREATE TABLE IF NOT EXISTS players 
+        (PLAYER_NAME varchar(255),
+        TEAM_ID int,
+        PLAYER_ID int,
+        SEASON int)
+        """)
+        
+    GET_ALL_PLAYERS = "SELCT * FROM nba_players"
 
-GET_ALL_PLAYERS = "SELCT * FROM nba_players"
-
-
-
+    INSERT_PLAYER = "INSERT INTO players (player_name, team_id, player_id, season) VALUES (%s, %s, %s) "
 
 
