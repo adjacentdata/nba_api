32896585371c1ef36266dda6f05120794dc20c9f
AJ Perez <atpcodeio@gmail.com>
2022-11-19 00:34:33 -0800

    Added Docstring to load data function

--- etl_script.py
+++ etl_script.py
@@ -4,10 +4,21 @@ import os
 connection_link = psycopg2.connect(os.getenv("DB_LINK"))
 cursor = connection_link.cursor()
 
+fetch_one = cursor.fetchone()
+fethc_all = cursor.fetchall()
+
+
 def load_data_from_csv():
-    players = open('project_files/nba_data/players.csv')
-    cursor.copy_from(players, 'players', sep=',' )
-    players.close()
-    cursor.close()
+    """
+    The function reads data from a csv file and auto loads it into the PostgreSQL server. 
+    """
+    with open('project_files/nba_data/players.csv', 'r') as data: 
+        next(data)
+        cursor.copy_from(data, 'players', sep=',' )
+
+cursor.commit()
+cursor.close()
+
+
     
  
