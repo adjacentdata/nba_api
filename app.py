d01806e223cf008ca42f846fd1ea0cc970607440
AJ Perez <atpcodeio@gmail.com>
2022-11-19 00:55:00 -0800

    Moved connection and cursors to app

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from dotenv import load_dotenv
+import psycopg2
+import os 
+import queries
 
 load_dotenv()
 
 app = Flask(__name__)
+
+connection_link = psycopg2.connect(os.getenv("DB_LINK"))
+cursor = connection_link.cursor()
+fetch_one = cursor.fetchone()
+fethc_all = cursor.fetchall()
+
 
 @app.route("/")
 def hello_world():
--- etl_script.py
+++ etl_script.py
@@ -4,10 +4,6 @@ import os
 connection_link = psycopg2.connect(os.getenv("DB_LINK"))
 cursor = connection_link.cursor()
 
-fetch_one = cursor.fetchone()
-fethc_all = cursor.fetchall()
-
-
 def load_data_from_csv():
     """
     The function reads data from a csv file and auto loads it into the PostgreSQL server. 
