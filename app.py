d5e100db51a6a01090000845fd507f1076a14d82
AJ Perez <atpcodeio@gmail.com>
2022-11-20 13:01:50 -0800

    added DB connection to main app

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-connection_link = psycopg2.connect(os.getenv("DB_LINK"))
+db_user=os.getenv('USER')
+db_password=os.getenv('DB_PASSWORD')
+connection_link = psycopg2.connect(database='nba_api', user=db_user, password=db_password, host='127.0.0.1', port='5432')
 cursor = connection_link.cursor()
+
 fetch_one = cursor.fetchone()
 fetch_all = cursor.fetchall()
 
