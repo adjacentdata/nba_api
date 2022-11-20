2c07f56db7c8b9c0228193e63f8ea011f5a0d15a
AJ Perez <atpcodeio@gmail.com>
2022-11-20 09:33:01 -0800

    added pathways to csv files

--- load_script.py
+++ load_script.py
@@ -2,6 +2,8 @@ import psycopg2
 import os 
 from queries import Players
 
+pathways = {'project_files/nba_data/games_details.csv', 'project_files/nba_data/games.csv', 'project_files/nba_data/players.csv', 'project_files/nba_data/ranking.csv', 'project_files/nba_data/teams.csv'}
+
 db_user=os.getenv('USER')
 db_password=os.getenv('DB_PASSWORD')
 connection_link = psycopg2.connect(database='nba_api', user=db_user, password=db_password, host='127.0.0.1', port='5432')
@@ -11,9 +13,12 @@ def create_table():
     cursor.execute(Players.CREATE_NBA_PLAYERS_TABLE)
     connection_link.commit()
 
-with open('project_files/nba_data/players.csv', 'r') as data: 
-        next(data)
-        cursor.copy_from(data, 'players', sep=',' )
+def load_data_into_db(pathways):
+    for i in range(len(pathways)):
+        with open(pathways[i], 'r') as data: 
+                next(data)
+                cursor.copy_from(data, 'players', sep=',' )
+    connection_link.commit()
 
 connection_link.commit()
 cursor.close()
