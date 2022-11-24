6c2f7a332043e7283100a563c90995a1bd8ec55f
AJ Perez <atpcodeio@gmail.com>
2022-11-24 10:01:50 -1000

    Added connection link

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ db_password=os.getenv('DB_PASSWORD')
 connection_link = psycopg2.connect(database='nba_api', user=db_user, password=db_password, host='127.0.0.1', port='5432')
 cursor = connection_link.cursor()
 
-
+@app.get('/players')
+def get_all_players():
+    with connection_link:
+        with connection_link.cursor() as cur:
+            pass
 
 @app.route("/")
 def hello_world():
